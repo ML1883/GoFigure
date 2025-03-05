51b8e1a285259da58cd74d1eaaa735ad04d36473
ML1883 <mitch.loedeman@proton.me>
2025-03-05 22:39:21 +0100

    Adjust file names, added some code to letteranalyzer Build up count letter function, rename the file. Rename some vars in the parser file and add better print statements

--- pkg/parser/parser.go
+++ pkg/parser/parser.go
@@ -6,18 +6,18 @@ import (
 	"unicode"
 )
 
-func ParseStringToAlphanumeric(input string) string {
+func ParseStringToAlphanumeric(textToParse string) string {
 	//Parses a string to Alphanumeric characters and spaces only.
 	var result strings.Builder
 
-	for index, char := range input {
+	for index, char := range textToParse {
 		// Allow alphanumeric characters and spaces
 		if unicode.IsLetter(char) || unicode.IsNumber(char) || unicode.IsSpace(char) {
 			result.WriteRune(char)
-			fmt.Printf("Character appended: %v", index)
+			fmt.Printf("Character appended: %v at index %v\n", char, index)
 		}
 	}
 
-	fmt.Printf("Final result: %v", result.String())
+	fmt.Printf("Final result: %v\n", result.String())
 	return result.String()
 }
