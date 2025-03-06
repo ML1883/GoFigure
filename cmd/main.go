679d1b7152a8279b7b1b39bd52b1d4febffc20e5
ML1883 <ml1883@pm.me>
2025-03-06 21:45:51 +0100

    Added vector multiplication and fixed lettercount function Lettercount now determines the value of letters relative to a and 0. Simple vector multiplication function for integers is added which will come in handy later.

--- cmd/main.go
+++ cmd/main.go
@@ -8,7 +8,17 @@ import (
 
 func main() {
 	fmt.Println("Starting main script.")
-	var parsed_text = parser.ParseStringToAlphanumeric("This is a test string")
-	var return_struct = analyzer.CountLetters(parsed_text)
-	fmt.Printf("Returned struct: %v\n", return_struct)
+	var parsedText = parser.ParseStringToAlphanumeric("This is a test stringz0123")
+	var parsedText2 = parser.ParseStringToAlphanumeric("This is a test stringz0123")
+	var returnStruct = analyzer.CountLetters(parsedText)
+	var returnStruct2 = analyzer.CountLetters(parsedText2)
+	totalValue, err := analyzer.IntVectorMultiplication(returnStruct.LetterNumberArray[:], returnStruct2.LetterNumberArray[:])
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Printf("Returned struct: %v\n Return totalcount: %v\n", returnStruct.LetterNumberArray, returnStruct.TotalCount)
+		fmt.Printf("Returned struct2: %v\n Return totalcount2: %v\n", returnStruct2.LetterNumberArray, returnStruct2.TotalCount)
+		fmt.Printf("Total value %v\n", totalValue)
+	}
+
 }
--- pkg/analyzer/letteranalyzer.go
+++ pkg/analyzer/letteranalyzer.go
@@ -6,8 +6,8 @@ import (
 )
 
 type lettercount struct {
-	totalcount        int
-	letterNumberArray [36]int //0-9 + 26 letters
+	TotalCount        int
+	LetterNumberArray [36]int //0-9 + 26 letters
 }
 
 func CountLetters(textToCount string) *lettercount {
@@ -17,18 +17,36 @@ func CountLetters(textToCount string) *lettercount {
 		if unicode.IsLetter(char) || unicode.IsNumber(char) {
 			if unicode.IsLetter(char) {
 				var letterLower rune = unicode.ToLower(char)
-				var letterNumber int = int(letterLower) //This is not correct for our purpose.
-				lcText.letterNumberArray[letterNumber+10]++
-				fmt.Printf("For index %v, plussed position %v with one with character %v", index, letterNumber+10, char)
+				var letterNumber int = int(letterLower - 'a')
+				lcText.LetterNumberArray[letterNumber+10]++
+				fmt.Printf("For index %v, plussed position %v with one with character %v\n", index, letterNumber+10, char)
 			} else {
-				lcText.letterNumberArray[int(char)]++
-				fmt.Printf("For index %v, plussed position %v with one with character %v", index, int(char), char)
+				lcText.LetterNumberArray[int(char-'0')]++
+				fmt.Printf("For index %v, plussed position %v with one with character %v\n", index, int(char), char)
 
 			}
 
 		}
-		lcText.totalcount++
+		lcText.TotalCount++
 	}
 
 	return &lcText
+}
+
+func IntVectorMultiplication(array1 []int, array2 []int) (int, error) {
+	//Optimized function for vector multiplication
+	var lengthArray1 int = len(array1)
+	var lengthArray2 int = len(array2)
+
+	if lengthArray1 > 0 && lengthArray2 > 0 && lengthArray1 == lengthArray2 {
+		var total int = 0
+		for index := range array1 {
+			total += (array1[index] * array2[index])
+			fmt.Printf("Array 1 value: %v and array2 value: %v\n", array1[index], array2[index])
+		}
+		return total, nil
+	} else {
+		return 0, fmt.Errorf("array with zero length or unequal array length detected. Length array1: %v Length array2: %v", lengthArray1, lengthArray2)
+	}
+
 }
