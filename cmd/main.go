2ea0b3fcd824319830cb224f909c2e4dfa6efbeb
ML1883 <ml1883@pm.me>
2025-03-14 17:29:09 +0100

    Added working normalization and corrected the average-of-averages in the position difference function

--- cmd/main.go
+++ cmd/main.go
@@ -8,24 +8,25 @@ import (
 
 func main() {
 	fmt.Println("Starting main script.")
-	var parsedText = parser.ParseStringToAlphanumeric("This is a test string123aaaa")
-	var parsedText2 = parser.ParseStringToAlphanumeric("aaaaThis is a test string123")
-	// var parsedText2 = parser.ParseStringToAlphanumeric("How this does is the a cosine test react stringz0123 to changing order") //Jaccard takes a tougher hit than Cosine in this case. Jaccard is probs more sensitive to outliers.
+	var parsedText = parser.ParseStringToAlphanumeric("         aaaa")
+	var parsedText2 = parser.ParseStringToAlphanumeric("aaaa         ")
+	// var parsedText = parser.ParseStringToAlphanumeric("This is a test string123aaaa")
+	// var parsedText2 = parser.ParseStringToAlphanumeric("aaaaThis is a test string123")
 	var returnStruct = analyzer.CountLetters(parsedText)
 	var returnStruct2 = analyzer.CountLetters(parsedText2)
-	totalValue, err := analyzer.IntVectorMultiplication(returnStruct.LetterNumberArray[:], returnStruct2.LetterNumberArray[:])
-	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		fmt.Printf("Returned struct: %v\n Return totalcount: %v\n Return position array: %v\n", returnStruct.LetterNumberArray, returnStruct.TotalCount, returnStruct.PositionArray)
-		fmt.Printf("Returned struct2: %v\n Return totalcount2: %v\n Return position array2: %v\n", returnStruct2.LetterNumberArray, returnStruct2.TotalCount, returnStruct2.PositionArray)
-		fmt.Printf("Total value %v\n", totalValue)
-	}
+	// totalValue, err := analyzer.IntVectorMultiplication(returnStruct.LetterNumberArray[:], returnStruct2.LetterNumberArray[:])
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// } else {
+	fmt.Printf("Returned struct: %v\n Return totalcount: %v\n Return position array: %v\n", returnStruct.LetterNumberArray, returnStruct.TotalCount, returnStruct.PositionArray)
+	fmt.Printf("Returned struct2: %v\n Return totalcount2: %v\n Return position array2: %v\n", returnStruct2.LetterNumberArray, returnStruct2.TotalCount, returnStruct2.PositionArray)
+	// fmt.Printf("Total value %v\n", totalValue)
+	// }
 	cosineSimilarity := analyzer.CosineSimilarityVectors(returnStruct.LetterNumberArray[:], returnStruct2.LetterNumberArray[:])
 	JaccardIndex := analyzer.JaccardIndexVectors(returnStruct.LetterNumberArray[:], returnStruct2.LetterNumberArray[:])
 	fmt.Printf("Cosine similairity of the two arrays: %v\n", cosineSimilarity)
 	fmt.Printf("Jaccard index of the two arrays: %v\n", JaccardIndex)
-	positionalCalc := analyzer.PositionDifferenceVectors(returnStruct.PositionArray[:], returnStruct2.PositionArray[:], max(returnStruct.TotalCount, returnStruct2.TotalCount))
+	positionalCalc := analyzer.PositionDifferenceVectors(returnStruct.PositionArray[:], returnStruct2.PositionArray[:], returnStruct.TotalCount, returnStruct2.TotalCount)
 	fmt.Printf("Position index is: %v\n", positionalCalc)
 
 }
